2f109aa535de64cd85e7c70cf8100ab51c7f8e08
iamtonmoy0 <tonmoy2code@gmail.com>
2024-05-13 21:33:16 +0600

    Add basic structure for a crawler program
    This commit introduces the basic structure for a web crawler program. It includes:
    
    - Definition of two struct types: `star` and `movie`
    - A `main`

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+type star struct {
+	Name      string
+	Photo     string
+	JobTitle  string
+	BirthDate string
+	Bio       string
+	TopMovies string
+}
+type movie struct {
+	Title string
+	Year  string
+}
+
+func main() {
+	crawl(*month, *day)
+}
+
+func crawl(month int, day int) {
+
+}
