fba19c4db8c85970a29bbfe03cbf9d3478ff976a
iamtonmoy0 <tonmoy2code@gmail.com>
2024-05-13 22:04:08 +0600

    "Add flag options for month and day; use colly package to crawl IMDB for birthdays"
    This commit introduces two flag options, -month and -day, which allow the user to specify the month and day for which

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+
+	"github.com/gocolly/colly"
+)
+
 type star struct {
 	Name      string
 	Photo     string
@@ -14,9 +21,23 @@ type movie struct {
 }
 
 func main() {
+	month := flag.Int("month", 1, "Month to fetch birthdays for")
+	day := flag.Int("day", 1, "day to fetch birthday for")
+	flag.Parse()
 	crawl(*month, *day)
 }
 
 func crawl(month int, day int) {
+c:=colly.NewCollector(
+	colly.AllowedDomains("imdb.com","www.imdb.com")
+)
+infoCollector:=c.Clone()
+c.OnHTML(".mode-detail",func(e *colly.HTMLElement){
+	profileUrl:=e.ChildAttr("div.listener-item-image>a","href")
+	profileUrl=e.Request.AbsoluteURL(profileUrl)
+	infoCollector.Visit(profileUrl)
+})
 
+	startUrl := fmt.Sprintf("https://www.imdb.com/search/name/?birth monthday=%d-%d", month, day)
+	c.Visit(startUrl)
 }
